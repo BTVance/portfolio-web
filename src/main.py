a19291a20c27bb9205249d2e80da9c1beb0333bf
BTVance <blaketannehill@gmail.com>
2025-12-17 15:27:00 -0800

    updated main and generated a webpage with markdown

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 from textnode import TextNode
 from textnode import TextType
 from copystatic import copy_recursive
+from gencontent import generate_page
 import shutil
 import os
 def main():
-    text = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text)
-
     if os.path.exists("public"):
         shutil.rmtree("public")
     os.mkdir("public")
     copy_recursive("static", "public")
+    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
 if __name__ == "__main__":
     main()
