4bc4fbe28f58634004d42f3be036b691ab2e9fc9
BTVance <blaketannehill@gmail.com>
2025-12-18 17:34:53 -0800

    implemented page switching and different web pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,4 +1,5 @@
 from inline_markdown import markdown_to_html_node
+from pathlib import Path
 import os
 def extract_title(markdown):
     new_lines = markdown.splitlines()
@@ -27,5 +28,21 @@ def generate_page(from_path, template_path, dest_path):
     
     with open(dest_path, "w") as f:
         f.write(page)
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    os.makedirs(dest_dir_path, exist_ok=True)
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        to_path = os.path.join(dest_dir_path, filename)
+
+        if os.path.isfile(from_path) and from_path.endswith(".md"):
+            html_path = Path(to_path).with_suffix(".html")
+            generate_page(from_path, template_path, html_path)
+        elif os.path.isdir(from_path):
+            generate_pages_recursive(from_path, template_path, to_path)
+
+
+
+
+
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from textnode import TextNode
 from textnode import TextType
 from copystatic import copy_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 import shutil
 import os
 def main():
@@ -9,6 +9,6 @@ def main():
         shutil.rmtree("public")
     os.mkdir("public")
     copy_recursive("static", "public")
-    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
+    generate_pages_recursive(dir_path_content="content/", template_path="template.html", dest_dir_path="public/")
 if __name__ == "__main__":
     main()
